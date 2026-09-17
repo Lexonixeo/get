00af9bae4506e276b84e63b00fd06d6bdebb5f56
Novikov Alexander <lexonix@list.ru>
2026-09-17 16:43:52 +0300

    3.5

---
+++ get-led/auto-light.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+led = 26
+GPIO.setup(led, GPIO.OUT)
+
+photo = 6
+GPIO.setup(photo, GPIO.IN)
+
+while True:
+    GPIO.output(led, not GPIO.input(photo))
