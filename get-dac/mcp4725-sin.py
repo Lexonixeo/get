41961aec7456a07b7a3de809af2cea6513d96a1f
Novikov Alexander <lexonix@list.ru>
2026-09-24 17:35:08 +0300

    Сделана ЦАП-7

---
+++ get-dac/mcp4725-sin.py
@@ -0,0 +1,18 @@
+import mcp4725_driver as mcp
+import signal_generator as sg
+import time
+
+amplitude = 3.3
+signal_frequency = 10
+sampling_frequency = 1000
+
+
+try:
+    dac = mcp.MCP4725(4.221, verbose=False)
+    
+    while True:
+        dac.set_voltage(amplitude * sg.get_sin_wave_amplitude(signal_frequency, time.time()))
+        sg.wait_for_sampling_period(sampling_frequency)
+
+finally:
+    dac.deinit()
